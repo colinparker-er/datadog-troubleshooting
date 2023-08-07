4284bed57279612c00aa91debc93a3fe0f8c3430
Colin Parker <cparker@extremereach.com>
2023-08-07 11:34:00 -0400

    Initial commit.

---
+++ DatadogApi.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using DatadogApi.ServiceModel;
+
+namespace DatadogApi.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
